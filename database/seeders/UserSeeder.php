f677416f320d6a9a3fce90e73e534796b01244c9
Vương Khánh Nhân <vuongkhanhnhan@gmail.com>
2025-11-21 21:27:36 +0700

    change password to @12345Password

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,7 +22,7 @@ class UserSeeder extends Seeder
             'first_name' => 'Super',
             'last_name' => 'Admin',
             'email' => 'admin@shopnest.com',
-            'password' => Hash::make('Password@123'),
+            'password' => Hash::make('@12345Password'),
         ]);
         $admin->roles()->attach($adminRole);
 
@@ -32,7 +32,7 @@ class UserSeeder extends Seeder
             'first_name'=>'Test',
             'last_name'=>'Seller',
             'email'=>'testseller@shopnest.com',
-            'password'=>Hash::make('Password@123'),
+            'password'=>Hash::make('@12345Password'),
         ]);
         $seller->roles()->attach($sellerRole);
 
@@ -41,7 +41,7 @@ class UserSeeder extends Seeder
             'first_name' => 'Test',
             'last_name' => 'Shipper',
             'email' => 'testshipper@shopnest.com',
-            'password' => Hash::make('Password@123'),
+            'password' => Hash::make('@12345Password'),
         ]);
         $shipper->roles()->attach($shipperRole);
 
